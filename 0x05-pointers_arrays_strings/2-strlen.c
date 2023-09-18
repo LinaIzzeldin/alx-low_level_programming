1602ff4bd201e77f58440c391fc8802b15df658e
LinaIzzeldin <linnnaizzeldin@gmail.com>
2023-09-18 15:33:36 -0700

    li \clear

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_strlen - returns the length of a string
+ * @s: string
+ *Return: returns lenght;
+ */
+int _strlen(char *s)
+{
+int sum, incrementation;
+
+incrementation = 0;
+for (sum = 0; s[sum] != '\0'; sum++)
+incrementation++;
+
+return (inc);
+}
