cc1643c4b62f34c935fc9eab0ec9b65a919a1826
LinaIzzeldin <linnnaizzeldin@gmail.com>
2023-09-25 13:52:47 -0700

    li

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _memcpy - prints buffer in hexa
+ * @dest: memory area
+ * @src: constant byte
+ * @n: the size of the memory to print
+ *
+ * Return: pointer to dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++ , n--)
+	{
+		dest[x] = src[x];
+	}
+	return (dest);
+}
+
