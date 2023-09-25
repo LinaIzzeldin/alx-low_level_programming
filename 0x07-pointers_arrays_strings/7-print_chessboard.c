cd3605c324b79e8c1a0c3e218fbc4998309b9821
LinaIzzeldin <linnnaizzeldin@gmail.com>
2023-09-25 14:19:13 -0700

    li

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints chessboard
+ * @a: pointer
+ *
+ *
+ * Return: 0
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int k, l;
+	int x = sizeof(*a) / sizeof(*a[0]);
+
+	for (k = 0; k < x; k++)
+	{
+		for (l = 0; l < x; l++)
+		{
+			_putchar(a[k][l]);
+		}
+		_putchar('\n');
+	}
+}
